0bf30a737fb53e2b6dc153594106c10a73fcdda5
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-05-18 22:46:51 -0600

    Time: 0 ms (100%), Space: 18 MB (12.31%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        #hash_set = set(target)
+        for i in range(1,target[-1]+1):
+            if i in target:
+                res.append("Push")
+            else:
+                res.append("Push")
+                res.append("Pop")
+        
+        return res
+
+            
