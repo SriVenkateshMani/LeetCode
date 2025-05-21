70641ebcc855418d31d5af15946d9ea32c194092
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-05-21 10:32:33 -0600

    Time: 0 ms (100%), Space: 18.2 MB (66.2%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         res = 0
+        m = len(mat)//2
+        n = len(mat)-1
         for i in range(len(mat)):
-            #primary diagonal
             res += mat[i][i]
-            #secondary diagonal
-            res += mat[i][len(mat)-1-i]
+            res += mat[i][n-i]
+            
+        if len(mat) % 2 == 1:
+            res -= mat[m][m]
         
-        return res - (mat[len(mat)//2][len(mat)//2] if len(mat) % 2 else 0)         #ternary operator for negating the middle value
+        return res
