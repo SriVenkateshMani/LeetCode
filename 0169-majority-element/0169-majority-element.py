722b62346143b04a6039e3bfd228bebb169d25c1
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-07-19 15:53:19 -0600

    Time: 10 ms (31.13%), Space: 19.3 MB (86.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count_map = {}
-        max_element = 0
-        max_count = 0
+        count_map = defaultdict(int)
+        max_val = 0
+        res = 0
         for i in nums:
-            count_map[i] = count_map.get(i,0) + 1
-
-        for key,value in count_map.items():
-            if value > max_count:
-                max_count = value
-                max_element = key
+            count_map[i] += 1
         
-
-        return max_element
-
-                
-
-        
+        for key, value in count_map.items():
+            max_val = max(max_val, value)
+            if count_map[key] == max_val:
+                res = key 
+            
+        return res
