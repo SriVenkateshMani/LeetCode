b310421eda869652c743930d0058fbcaef00ca00
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-07-20 17:18:41 -0600

    Time: 15 ms (9.62%), Space: 20.8 MB (89.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,6 +6,6 @@ class Solution:
             if not res or res[-1][1] < i[0]:
                 res.append(i)
             else:
-                res[-1] = [res[-1][0], max(res[-1][1], i[1])]
+                res[-1] = [min(res[-1][0],i[0]), max(res[-1][1], i[1])]
         
         return res
