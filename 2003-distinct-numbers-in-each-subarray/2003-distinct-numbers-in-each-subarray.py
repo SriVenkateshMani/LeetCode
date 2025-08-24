f24f28d145cbfdb33b46c68817fa12609a880c7c
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-08-24 16:31:16 -0600

    Time: 935 ms (49.12%), Space: 37.8 MB (90.35%) - LeetHub

---
+++ 2003-distinct-numbers-in-each-subarray/2003-distinct-numbers-in-each-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def distinctNumbers(self, nums: List[int], k: int) -> List[int]:
+        i,j = 0, 0
+        n = len(nums)
+        count_map = defaultdict(int)
+        res = []
+
+        while j < n:
+            count_map[nums[j]] += 1 
+            
+            if j-i+1 > k:
+                count_map[nums[i]] -= 1
+                if count_map[nums[i]] == 0:
+                    del count_map[nums[i]] 
+                i += 1
+
+            if j-i+1 == k:
+                res.append(len(count_map))
+
+            j += 1
+        
+        return res
+
+            
