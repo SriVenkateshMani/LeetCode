c4860231940a887b7ec7ef0ccd03d13a815bc957
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-09-19 12:22:33 -0600

    Time: 0 ms (100%), Space: 29.5 MB (80.81%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        max_el = float("-inf")
+        l, r = 0, len(arr)-1
+
+        while l < r:
+            mid = (l+r) // 2
+            if arr[mid] > arr[mid+1]:
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
