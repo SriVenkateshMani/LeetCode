99d19d2ab9dc571541a62e753fd6d8a0418aa8b7
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-09-26 15:49:27 -0600

    Time: 151 ms (27.62%), Space: 37.3 MB (41.59%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,21 +1,22 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        count_map = {}
-        max_sum = 0
-        cur_sum = 0
-        l = 0
-        for r in range(len(nums)):
-            cur_sum += nums[r]
-            count_map[nums[r]] = count_map.get(nums[r],0) + 1
+        i = j = max_sum = 0
+        count_map = defaultdict(int)
+        n = len(nums)
+        run_sum = 0
+        while j < n:
+            run_sum += nums[j]
+            count_map[nums[j]] += 1
 
-            if r-l+1 > k:
-                count_map[nums[l]] -= 1
-                cur_sum -= nums[l]
-                if count_map[nums[l]] == 0:
-                    count_map.pop(nums[l])
-                l += 1
+            if j-i+1 > k:
+                count_map[nums[i]] -= 1
+                run_sum -= nums[i]
+                if count_map[nums[i]] == 0:
+                    del count_map[nums[i]]
+                i += 1
+        
+            if len(count_map) == k and j-i+1 == k:
+                max_sum = max(max_sum, run_sum)
+            j += 1
             
-            if len(count_map) == k and r-l+1 == k:
-                max_sum = max(max_sum, cur_sum)
-
         return max_sum
