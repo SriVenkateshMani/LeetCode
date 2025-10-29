71c0cd9514807e6d9f3ca0139fee89173333ccfd
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-10-29 17:09:06 -0600

    Time: 66 ms (96.09%), Space: 47.8 MB (56.72%) - LeetHub

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        res = [-1, -1]
+        first_cp = 0
+        prev_cp = 0
+        curr_cp = 0
+        min_dist = float("inf")
+        prev = head
+        curr = prev.next
+        fow = curr.next
+        pos = 1
+
+        while fow:
+            if (prev.val < curr.val and fow.val < curr.val) or (prev.val > curr.val and fow.val > curr.val):
+                curr_cp = pos
+                if first_cp == 0:
+                    first_cp = curr_cp
+                else:
+                    min_dist = min(min_dist, curr_cp - prev_cp)
+                
+                prev_cp = curr_cp
+
+            prev = curr
+            curr = fow
+            fow = fow.next
+            pos += 1
+
+        if curr_cp != 0 and first_cp != 0 and first_cp != curr_cp:
+            max_dist = curr_cp - first_cp
+            res[1] = max_dist
+
+        if min_dist != float("inf"):
+            res[0] = min_dist
+        
+        return res
+        
