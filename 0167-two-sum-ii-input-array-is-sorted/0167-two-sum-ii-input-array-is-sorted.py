833bac4bd2eaa4b7767aa4a723894e1fee25ba50
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-11-07 17:13:27 -0700

    Time: 3 ms (81.02%), Space: 18.6 MB (59%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -7,9 +7,9 @@ class Solution:
             if numbers[l] + numbers[r] == target:
                 return [l+1, r+1]
             
-            elif numbers[l] + numbers[r] <= target:
+            elif numbers[l] + numbers[r] < target:
                 l += 1
             
-            elif numbers[l] + numbers[r] >= target:
+            elif numbers[l] + numbers[r] > target:
                 r -= 1
 
