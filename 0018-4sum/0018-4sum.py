b080ec2255dd73c36ed5920c3adc7cdb6d93fc88
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-11-07 21:12:42 -0700

    Time: 387 ms (63.51%), Space: 17.7 MB (82.46%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,11 +3,11 @@ class Solution:
         res = []
         nums.sort()
 
-        for i in range(len(nums)):
+        for i in range(len(nums)-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            for j in range(i+1, len(nums)):
+            for j in range(i+1, len(nums)-2):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
                 
