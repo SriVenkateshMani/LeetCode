3097a0721602998be934a94a71e8625b25b47619
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-11-15 20:05:08 -0700

    Time: 0 ms (100%), Space: 18.1 MB (66.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,47 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        def binary_search(nums, target, l, r):
-            while l <= r:
-                mid = (l + r) // 2
-                if nums[mid] < target:
-                    l = mid + 1
-                elif nums[mid] > target:
-                    r = mid - 1
-                else:
-                    return mid
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            m = (l+r) // 2
+            if target == nums[m]:
+                return m
             
-            return -1
-
-        def findPivot(nums):
-            l, r = 0, len(nums)-1
-            while l <= r:
-                mid = (l + r) // 2
-                if mid < r and nums[mid] > nums[mid + 1]:
-                    return mid
-                elif mid > l and nums[mid] < nums[mid - 1]:
-                    return mid - 1
-                elif nums[mid] <= nums[l]:
-                    r = mid - 1
+            if nums[0] <= nums[m]:
+                if target < nums[0] or target > nums[m]:
+                    l = m + 1
                 else:
-                    l = mid + 1
-
-            return -1
-        
-        
-        pivot = findPivot(nums)
-
-        if pivot == -1:
-            return binary_search(nums, target, 0, len(nums)-1)
-        
-        elif nums[pivot] == target:
-            return pivot
+                    r = m - 1
             
-        elif target >= nums[0]:
-            return binary_search(nums, target, 0, pivot-1)
-
-        else:
-            return binary_search(nums, target, pivot+1, len(nums)-1)
+            else:
+                if target > nums[r] or target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
         
-        
-
+        return -1
