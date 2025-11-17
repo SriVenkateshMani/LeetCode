d2f5021491996f1c361c89e5e3e9d6233258c26c
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-11-17 16:39:44 -0700

    Time: 131 ms (42.4%), Space: 75.4 MB (15.66%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,9 +1,11 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = defaultdict(list)
+        self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
         self.store[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
