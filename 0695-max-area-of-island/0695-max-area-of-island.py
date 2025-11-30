1d27e2560437f5e1b2d7a2edeafc5342c56a3b27
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-11-30 16:37:00 -0700

    Time: 20 ms (61.69%), Space: 18.3 MB (65.99%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,17 +3,25 @@ class Solution:
         n, m = len(grid), len(grid[0])
 
         def dfs(i,j):
+            nonlocal count
             if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0:
-                return 0
+                return None
             else:
                 grid[i][j] = 0
-                return 1 + (dfs(i-1,j) + dfs(i+1,j) + dfs(i,j+1) + dfs(i,j-1))   
-        
+                count += 1
+                dfs(i-1,j)
+                dfs(i+1,j)
+                dfs(i,j+1)
+                dfs(i,j-1)  
+                
+            return count
+
         max_islands = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
-                    area = dfs(i,j)
-                    max_islands = max(max_islands, area)
+                    count = 0
+                    dfs(i,j)
+                    max_islands = max(max_islands, count)
                     
         return max_islands
