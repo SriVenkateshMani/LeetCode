cdddf94469dce60cd652ef30ec6f2be955f2799b
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2025-12-21 21:55:51 -0700

    Time: 11 ms (91.87%), Space: 18.2 MB (40.71%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,8 +1,3 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
-        freq_map = defaultdict(int)
-
-        for i in s:
-            freq_map[i] += 1
-    
-        return len(freq_map)
+        return len(set(s))
