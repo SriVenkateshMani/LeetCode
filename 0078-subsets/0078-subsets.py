a33717308b82917b96831dd8459e635cf8774d90
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2026-01-07 10:47:32 -0700

    Time: 0 ms (100%), Space: 19.5 MB (14.37%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,21 +2,20 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         res = []
-        curr = []
 
-        def backtrack(i):
+        def backtrack(i, path):
             if i == n:
-                res.append(curr[:])
+                res.append(path[:])
                 return None
             
             
             # Dont pick
-            backtrack(i+1)
+            backtrack(i+1, path)
 
             # Pick nums[i]
-            curr.append(nums[i])
-            backtrack(i+1)
-            curr.pop()
+            path.append(nums[i])
+            backtrack(i+1, path)
+            path.pop()
             
-        backtrack(0)
+        backtrack(0, [])
         return res
