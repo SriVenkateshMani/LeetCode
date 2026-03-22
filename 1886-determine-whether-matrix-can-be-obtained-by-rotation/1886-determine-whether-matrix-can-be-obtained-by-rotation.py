44c60837f9b5968484cb528fdc34986e517f41ee
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2026-03-22 16:50:58 -0600

    Time: 4 ms (5.41%), Space: 19.4 MB (23.68%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,21 +1,25 @@
 class Solution:
-    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        
-        def rotate(mat):
-            n = len(mat)
+    def findRotation(
+        self, mat: List[List[int]], target: List[List[int]]
+    ) -> bool:
+        n = len(mat)
+        # at most 4 rotations
+        for k in range(4):
+            # rotation operation
+            for i in range(n // 2):
+                for j in range((n + 1) // 2):
+                    (
+                        mat[i][j],
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                    ) = (
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                        mat[i][j],
+                    )
 
-            for i in range(n-1):
-                for j in range(i+1, n):
-                    mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-            
-            for i in range(n):
-                mat[i].reverse()
-            
-            return mat
-        
-        for i in range(4):
-            if rotate(mat) == target:
+            if mat == target:
                 return True
-
         return False
-            
