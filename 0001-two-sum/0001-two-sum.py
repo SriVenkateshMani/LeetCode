f874628ec35b336267ff2ec075a0898309dd10a8
Sri venkatesha mani <148737935+SriVenkateshMani@users.noreply.github.com>
2026-08-30 11:14:25 -0500

    Time: 1756 ms (13.53%), Space: 19.8 MB (82.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        freq_map = defaultdict(int)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
-        for i, v in enumerate(nums):
-            if (target - v) in freq_map:
-                return [freq_map[target - v], i]
-            freq_map[v] = i
+        return -1
